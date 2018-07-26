5a8155b9f35b61734e683e712f23d5d5ebb07e8a
Radek Semančík <radek.semancik@ogilvydevelopment.cz>
2018-07-26 12:00:05 +0200

    global file system

--- www/index.php
+++ www/index.php
@@ -1,5 +1,13 @@
 <?php
 
+// absolute filesystem path to the web root
+define('WWW_DIR', dirname(__FILE__));
+
+// absolute filesystem path to the application root
+define('FILES_DIR', WWW_DIR . '/files');
+
+define('IMAGES_DIR', WWW_DIR . '/images');
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType(Nette\Application\Application::class)
